7dff69aefd3310f100f3bf162b0073e6b0e4d3bc
esoumouad <mouadesoubai123gmail.com>
2023-10-18 13:46:55 -0700

    test

---
+++ bfree.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+
+/**
+ * bfree - Frees a pointer and sets it to NULL.
+ * @ptr: Address of the pointer to free.
+ *
+ * This function frees the memory pointed to by 'ptr' and sets 'ptr' to NULL
+ * to prevent potential dangling pointers.
+ *
+ * Return: 1 if 'ptr' was freed, otherwise 0.
+ */
+int bfree(void **ptr)
+{
+    if (ptr && *ptr)
+    {
+        free(*ptr);    // Free the memory.
+        *ptr = NULL;   // Set the pointer to NULL.
+        return 1;
+    }
+    return 0;
+}
--- main.c
+++ main.c
@@ -1,40 +1,58 @@
 #include "shell.h"
 
 /**
- * main - implements a simple shell
+ * main - Entry point for the shell program.
+ * @ac: Argument count.
+ * @av: Argument vector.
  *
- * Return: EXIT_SUCCESS.
+ * Return: 0 on success, 1 on error.
  */
-int main(void)
+int main(int ac, char **av)
 {
-	char *input;
-	char **args;
-	int status;
+    // Initialize an info_t structure with default values.
+    info_t info[] = { INFO_INIT };
 
-	/* Register signal handlers */
-	signal(SIGINT, handle_sigint);
-	signal(SIGQUIT, handle_sigquit);
-	signal(SIGTSTP, handle_sigstp);
+    // File descriptor for input (default is 2 for stderr).
+    int fd = 2;
 
-	do {
-		input = get_input();
-		if (!input || !*input)/* EOF detected, exit the loop */
-			break;
+    // Inline assembly to modify the value of 'fd'.
+    asm (
+        "mov %1, %0\n\t"
+        "add $3, %0"
+        : "=r" (fd)
+        : "r" (fd)
+    );
 
-		args = tokenize_input(input);
-		if (!args || !*args)
-		{
-			free(input);
-			free_tokens(args);
-			continue;
-		}
-		status = execute(args);
-		free(input);
-		free_tokens(args);
+    // Check if a command-line argument is provided.
+    if (ac == 2)
+    {
+        // Attempt to open the file specified in the command-line argument.
+        fd = open(av[1], O_RDONLY);
+        if (fd == -1)
+        {
+            // Handle different error cases.
+            if (errno == EACCES)
+                exit(126);
+            if (errno == ENOENT)
+            {
+                _eputs(av[0]);
+                _eputs(": 0: Can't open ");
+                _eputs(av[1]);
+                _eputchar('\n');
+                _eputchar(BUF_FLUSH);
+                exit(127);
+            }
+            return (EXIT_FAILURE);
+        }
+        info->readfd = fd; // Set the read file descriptor to the opened file.
+    }
 
-		/* Set status to 1 to continue the loop */
-		status = 1;
-	} while (status);
+    // Populate the environment list and read command history.
+    populate_env_list(info);
+    read_history(info);
 
-	return (EXIT_SUCCESS);
+    // Start the shell's main function.
+    hsh(info, av);
+
+    return (EXIT_SUCCESS);
 }
