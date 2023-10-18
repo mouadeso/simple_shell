bfb849d54d797fd0a9a47b2fcc331adf93420aea
mouadeso <mouadesoubai123gmail.com>
2023-10-18 14:13:32 -0700

    test

--- main.c
+++ main.c
@@ -9,28 +9,18 @@
  */
 int main(int ac, char **av)
 {
-    // Initialize an info_t structure with default values.
     info_t info[] = { INFO_INIT };
-
-    // File descriptor for input (default is 2 for stderr).
     int fd = 2;
-
-    // Inline assembly to modify the value of 'fd'.
     asm (
         "mov %1, %0\n\t"
         "add $3, %0"
         : "=r" (fd)
         : "r" (fd)
     );
-
-    // Check if a command-line argument is provided.
-    if (ac == 2)
     {
-        // Attempt to open the file specified in the command-line argument.
         fd = open(av[1], O_RDONLY);
         if (fd == -1)
         {
-            // Handle different error cases.
             if (errno == EACCES)
                 exit(126);
             if (errno == ENOENT)
@@ -44,14 +34,12 @@ int main(int ac, char **av)
             }
             return (EXIT_FAILURE);
         }
-        info->readfd = fd; // Set the read file descriptor to the opened file.
+        info->readfd = fd; 
     }
 
-    // Populate the environment list and read command history.
     populate_env_list(info);
     read_history(info);
 
-    // Start the shell's main function.
     hsh(info, av);
 
     return (EXIT_SUCCESS);
